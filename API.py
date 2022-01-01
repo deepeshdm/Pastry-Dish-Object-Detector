8df893f543032e043cc8bb8358abcf9b38e7809b
deepeshdm <deepeshmhatre133@gmail.com>
2022-01-01 12:10:10 +0530

    added webcam detection feature

--- API.py
+++ API.py
@@ -206,3 +206,101 @@ def detect_objects(image_path, saved_model_path, labelMap_path, min_threshold=0.
     image_with_detections = np.array(image_with_detections)
 
     return image_with_detections, detected_objects
+
+
+# ------------------------------CODE RELATED TO WEBCAM OBJECT DETECTION------------------------------------#
+
+
+def process_frame(image, detect_function, labelMap_path, min_threshold=0.5):
+    """
+    Parameters
+    ----------
+    image : Numpy array Image frame
+    detect_function : Loaded model detection function
+    labelMap_path : Path to 'label_map.pbtxt' file
+    min_threshold : Minimum decision threshold for classification
+
+    Returns : Numpy array of output Image
+    -------
+    """
+
+    # -------------------------------------------------------------------------------
+
+    # LOAD LABEL MAP DATA FOR PLOTTING
+    category_index = label_map_util.create_category_index_from_labelmap(labelMap_path,
+                                                                        use_display_name=True)
+
+    # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
+    input_tensor = tf.convert_to_tensor(image)
+
+    # The model expects a batch of images, so add an axis with `tf.newaxis`.
+    input_tensor = input_tensor[tf.newaxis, ...]
+
+    # input_tensor = np.expand_dims(image_np, 0)
+    detections = detect_function(input_tensor)
+
+    # All outputs are batches tensors.
+    # Convert to numpy arrays, and take index [0] to remove the batch dimension.
+    # We're only interested in the first num_detections.
+    num_detections = int(detections.pop('num_detections'))
+    detections = {key: value[0, :num_detections].numpy()
+                  for key, value in detections.items()}
+    detections['num_detections'] = num_detections
+
+    # detection_classes should be ints.
+    detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+    # -------------------------------------------------------------------------------
+
+    # SET MIN_SCORE_THRESH BASED ON YOU MINIMUM THRESHOLD FOR DETECTIONS
+    viz_utils.visualize_boxes_and_labels_on_image_array(
+        image,
+        detections['detection_boxes'],
+        detections['detection_classes'],
+        detections['detection_scores'],
+        category_index,
+        use_normalized_coordinates=True,
+        max_boxes_to_draw=100,
+        min_score_thresh=min_threshold,
+        agnostic_mode=False)
+
+    return image
+
+
+def detect_webcam(detection_function, labelMap_path, min_threshold=0.5):
+
+    print("Starting webcam...")
+
+    # define a video capture object
+    vid = cv2.VideoCapture(0)
+
+    while (True):
+
+        # Capture the video frame by frame
+        ret, frame = vid.read()
+
+        # ------------------------------------------------------------
+
+        image = cv2.resize(frame, (416, 416))
+
+        # # Numpy array of output Image and Dictionary of Top 10 detected objects with scores
+        Output_Image = process_frame(image, detection_function, labelMap_path, min_threshold)
+
+        # ------------------------------------------------------------
+
+        # resizing the output image
+        frame = cv2.resize(Output_Image, (620, 620))
+
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+
+        # the 'q' button is set as the
+        # quitting button you may use any
+        # desired button of your choice
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # After the loop release the cap object
+    vid.release()
+    # Destroy all the windows
+    cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,23 +1,6 @@
-#
-# # Path to Input Image
-# IMAGE_PATH = 'C:/Users/dipesh/Desktop/fiverr-hasijayawardana/train/IMG_0008.JPG'
-#
-# # Path to pre-trained model
-# SAVED_MODEL_PATH = r'C:/Users/dipesh/Desktop/fiverr-hasijayawardana/Pastry Detection/saved_model'
-#
-# # Path to 'label_map.pbtxt' file
-# PATH_TO_LABELS = 'C:/Users/dipesh/Desktop/fiverr-hasijayawardana/Pastry Detection/label_map.pbtxt'
-#
-# # Object detection threshold
-# THRESHOLD = 0.5
-#
-# # Path to save the output Image with name and extension
-# SAVE_PATH = r"C:/Users/dipesh/Desktop/fiverr-hasijayawardana/outputs/output.jpeg"
-#
-# from API import detect_pastries
-# detect_pastries(IMAGE_PATH, SAVED_MODEL_PATH, PATH_TO_LABELS,THRESHOLD,SAVE_PATH)
+# ------------------------------DETECT AND DISPLAY IMAGE------------------------------------------ #
 
-# ------------------------------------------------------------------------------------------
+"""
 
 # Path to Input Image
 IMAGE_PATH = 'C:/Users/dipesh/Desktop/fiverr-hasijayawardana/train/IMG_0008.JPG'
@@ -34,6 +17,29 @@ THRESHOLD = 0.5
 # Path to save the output Image with name and extension
 SAVE_PATH = r"C:/Users/dipesh/Desktop/fiverr-hasijayawardana/outputs/output.jpeg"
 
+from API import detect_pastries
+detect_pastries(IMAGE_PATH, SAVED_MODEL_PATH, PATH_TO_LABELS,THRESHOLD,SAVE_PATH)
+
+"""
+
+# ------------------------------DETECT AND RETURN LIST------------------------------------------ #
+
+"""
+
+# Path to Input Image
+IMAGE_PATH = 'C:/Users/dipesh/Desktop/fiverr-hasijayawardana/train/IMG_0008.JPG'
+
+# Path to pre-trained model
+SAVED_MODEL_PATH = \
+    r'C:/Users/dipesh/Desktop/final_hasintha_jayawardana/fiverr-hasijayawardana-object-detector/saved_model'
+
+# Path to 'label_map.pbtxt' file
+PATH_TO_LABELS = \
+    r'C:/Users/dipesh/Desktop/final_hasintha_jayawardana/fiverr-hasijayawardana-object-detector/label_map.pbtxt'
+
+# Object detection threshold
+THRESHOLD = 0.5
+
 import cv2
 from API import detect_objects
 
@@ -43,3 +49,29 @@ Output_Image, Detected_Objects = detect_objects(IMAGE_PATH, SAVED_MODEL_PATH, PA
 print(Detected_Objects)
 cv2.imshow("OUTPUT", Output_Image)
 cv2.waitKey()
+
+"""
+
+# ------------------------------OPEN WEBCAM AND DETECT OBJECTS------------------------------------------ #
+
+# NOTE : Press "Q" on keyboard to close webcam.
+
+from API import detect_webcam
+import tensorflow as tf
+
+# Path to pre-trained model
+SAVED_MODEL_PATH = \
+    r'C:\Users\dipesh\Desktop\final_hasintha_jayawardana\fiverr-hasijayawardana-object-detector\saved_model'
+
+# Path to 'label_map.pbtxt' file
+PATH_TO_LABELS = \
+    r'C:\Users\dipesh\Desktop\final_hasintha_jayawardana\fiverr-hasijayawardana-object-detector\label_map.pbtxt'
+
+# Object detection threshold
+THRESHOLD = 0.5
+
+# LOAD SAVED MODEL AND BUILD DETECTION FUNCTION
+print("Loading model...this will take a minute")
+detect_fn = tf.saved_model.load(SAVED_MODEL_PATH)
+
+detect_webcam(detect_fn, PATH_TO_LABELS, THRESHOLD)
